3e82beb458b62bff9175da474b5900c7b94a63ac
Mohamed Agwa <magwa912@gmail.com>
2026-09-25 15:31:23 +0300

    Add initial project structure with API, Core, and Infrastructure layers

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities;
+using Infrastructure.Config;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class StoreContext : DbContext
+{
+    public StoreContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected StoreContext() { } 
+
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+    }
+}
